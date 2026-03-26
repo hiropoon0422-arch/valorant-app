1ef8cb2b7425c138e29176b15c888751b143e65b
hiropoon0422-arch <hiropoon.0422@gmail.com>
2026-03-27 01:52:26 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#　お祝いメッセージの表示
+messge = "pythonの世界へようこそ"
+print(messge)
+
+# 1から10までを合計してくれる計算（pythonは計算が得意！）
+number = range(1,11)
+total = sum(number)
+
+print(f"1から10までを全部足す...　{total} になります！")
+
+#1から100万まで足してみる
+print(f"ちなみに1から100万まで足すと...　{sum(range(1,1000001))}　です！")
+
+name = "pon"
+age = 33
+print(f"こんにちは！{name} さんは、来年{age+1}歳になりますね！")
